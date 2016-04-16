c4340c6af8235510a4725958c51d504381cb5769
Matt Powell <fauxparse@gmail.com>
2016-04-17 08:57:53 +1200

    Google OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :games
 
+  get "/auth/google/callback" => "sessions#create"
+  get "/logout" => "sessions#destroy", as: :logout
+  get "/login" => "sessions#new", as: :login
+
+  root to: "games#index"
+
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 end
